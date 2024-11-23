e19e4696c718ac196f4ecfe3aedb5d7ed9aab499
deelaksha <deelakshadeekshith@gmail.com>
2024-11-23 12:44:44 +0530

    with proper header section

--- src/app/Registerd_Players/page.tsx
+++ src/app/Registerd_Players/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { supabase } from '../../../lib/supabaseClient';
-import Navbar from '@/app/Owner/Header/page';
+import Navbar from '@/app/Header/page';
 import { Loader2, Search, UserX } from 'lucide-react';
 
 
